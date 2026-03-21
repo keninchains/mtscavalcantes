9b8b39e7f397f37c235c5124f2899a2e4d200ded
Matheus Cavalcante <mtscavalcantes.mc@outlook.com>
2026-03-21 12:13:59 -0300

    built home page prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,35 @@
+import { Logo } from "./components/Logo";
+import BorderGlow from "./components/ReactBits/BorderGlow";
+import { ArrowDownTrayOutline } from "./components/heroicons/ArrowDownTrayOutline";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="flex min-h-screen flex-col bg-mist-950 text-mist-50">
+      <header className="flex items-center justify-between p-4 px-56">
+        <div className="flex items-center gap-4">
+          <Logo className="size-16" />
+          <p className="text-2xl font-semibold">Matheus Cavalcante</p>
+        </div>
+        <div className="flex items-center">
+          <BorderGlow
+            backgroundColor="#161b1d"
+            borderRadius={12}
+            edgeSensitivity={10}
+          >
+            <a
+              href="/curriculo"
+              className="flex h-10 w-30 items-center justify-center gap-2"
+            >
+              <ArrowDownTrayOutline />
+              <p>Currículo</p>
+            </a>
+          </BorderGlow>
+        </div>
+      </header>
+      <div></div>
+      <footer className="mt-auto grid h-32 place-items-center">
+        <p className="text-sm">© 2026 · Matheus Cavalcante</p>
+      </footer>
+    </div>
+  );
 }
---
+++ src/components/Logo.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  className?: string;
+};
+
+export function Logo({ className }: Props) {
+  return (
+    <svg
+      width="100"
+      height="100"
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <g clip-path="url(#clip0_1_2)">
+        <path d="M4 46L50 0V50L0 100V0H4V46Z" fill="black" />
+        <path d="M100 100H96V54L50 100V50L100 0V100Z" fill="black" />
+      </g>
+      <defs>
+        <clipPath id="clip0_1_2">
+          <rect width="100" height="100" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
